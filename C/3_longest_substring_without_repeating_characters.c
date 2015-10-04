f3b34b9fab8a059d62258fc4cceb43e211815fdc
Shicheng XU <lightxuzju@gmail.com>
2015-10-04 11:15:59 -0400

    Finish problem 3.

---
+++ C/3_longest_substring_without_repeating_characters.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int lengthOfLongestSubstring(char* s) {
+    int word_flags[256];
+    char* head = s+1;
+    char* tail = s;
+    int current_len = 1;
+    int answer = 1;
+
+    if (*s == 0)
+    	return 0;
+
+    memset(word_flags, 0, sizeof(int)*256);
+    word_flags[*s] = TRUE;
+    
+    while (*head != 0)
+    {
+    	// printf("head = %s\n", head);
+    	// printf("tail = %s\n", tail);
+    	// printf("current_len = %d\n", current_len);
+    	// printf("answer = %d\n", answer);
+    	if (word_flags[*head] == 0)
+    	{
+    		word_flags[*head] = 1;
+    		current_len++;
+    	}
+    	else
+    	{
+    		// Update answer
+    		if(current_len > answer)
+    		{
+    			answer = current_len;
+    		}
+    		// update tail
+    		while (*tail != *head)
+    		{
+    			// printf("*head = %c\n", *head);
+    			// printf("*tail = %c\n", *tail);
+    			word_flags[*tail] = 0;
+    			current_len--;
+    			tail = tail + 1;
+    		}
+    		tail = tail + 1; 
+    	}
+    	head = head + 1;
+    }
+    if (current_len > answer)
+    {
+    	answer = current_len;
+    }
+    return answer;
+}
+
+int main()
+{
+	char* s = "au";
+	int answer = lengthOfLongestSubstring(s);
+	printf("answer = %d\n", answer);
+	return 0;
+}
